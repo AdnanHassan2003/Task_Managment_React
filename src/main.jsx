ee3235da64c58387751b6d7e2346987126c2fdda
AdnanHassan2003 <cadnaanhassan2003@gmail.com>
2024-07-26 00:48:40 +0300

    start project test managment

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./routes/index.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { StyleSheetManager } from "styled-components";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<Suspense fallback={<div>Loading...</div>}>
+		<BrowserRouter>
+			<StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+				<AppRoutes />
+			</StyleSheetManager>
+		</BrowserRouter>
+	</Suspense>
+);
